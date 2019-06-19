5b5f6f3250eca8a0fc0700fde120de989a370da3
DysonProject <Jasess01@hotmail.com>
2019-06-19 12:08:18 -0400

    Add files via upload

---
+++ DysonLevelManager.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using UnityEngine;
+using Cinemachine;
+using MoreMountains.CorgiEngine;
+using System.Linq;
+using UnityEngine.SceneManagement;
+using MoreMountains.Tools;
+using MoreMountains.MMInterface;
+
+public class DysonLevelManager : LevelManager
+{
+    /// <summary>
+    /// Initializes GUI stuff
+    /// </summary>
+    protected virtual void LevelGUIStart()
+    {
+        // set the level name in the GUI
+        LevelNameEvent.Trigger(SceneManager.GetActiveScene().name);
+        // fade in
+        MMFadeOutEvent.Trigger(IntroFadeDuration);
+    }
+
+    public struct LevelNameEvent
+    {
+        public string LevelName;
+
+        /// <summary>
+        /// Initializes a LevelNameEvent
+        /// </summary>
+        /// <param name="levelName"></param>
+        public LevelNameEvent(string levelName)
+        {
+            LevelName = levelName;
+        }
+
+        static LevelNameEvent e;
+        public static void Trigger(string levelName)
+        {
+            e.LevelName = levelName;
+            MMEventManager.TriggerEvent(e);
+        }
+    }
+
+
+    protected static new DysonLevelManager _instance;
+
+    public static new DysonLevelManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<DysonLevelManager>();
+                if (_instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    _instance = obj.AddComponent<DysonLevelManager>();
+                }
+            }
+            return _instance;
+        }
+    } 
+
+    protected override void Awake()
+    { 
+        if (!Application.isPlaying)
+        {
+            return;
+        }
+
+        _instance = this;
+
+        InstantiatePlayableCharacters();
+    }
+
+    public CinemachineVirtualCamera[] vCams;
+    /// 
+    /// Gets current camera, points number, start time, etc.
+    /// 
+    protected override void Initialization()
+    {
+        // storage
+        //LevelCameraController = FindObjectOfType();
+        for (int i = 0; i < vCams.Length; i++)
+        {
+            vCams[i].Follow = Players[0].transform;
+        }
+        _savedPoints = GameManager.Instance.Points;
+        _started = DateTime.UtcNow;
+
+        // we store all the checkpoints present in the level, ordered by their x value
+        if ((CheckpointAttributionAxis == CheckpointsAxis.x) && (CheckpointAttributionDirection == CheckpointDirections.Ascending))
+        {
+            Checkpoints = FindObjectsOfType<CheckPoint>().OrderBy(o => o.transform.position.x).ToList();
+        }
+        if ((CheckpointAttributionAxis == CheckpointsAxis.x) && (CheckpointAttributionDirection == CheckpointDirections.Descending))
+{
+            Checkpoints = FindObjectsOfType<CheckPoint>().OrderByDescending(o => o.transform.position.x).ToList();
+        }
+        if ((CheckpointAttributionAxis == CheckpointsAxis.y) && (CheckpointAttributionDirection == CheckpointDirections.Ascending))
+        {
+            Checkpoints = FindObjectsOfType<CheckPoint>().OrderBy(o => o.transform.position.y).ToList();
+        }
+        if ((CheckpointAttributionAxis == CheckpointsAxis.y) && (CheckpointAttributionDirection == CheckpointDirections.Descending))
+        {
+            Checkpoints = FindObjectsOfType<CheckPoint>().OrderByDescending(o => o.transform.position.y).ToList();
+        }
+        if ((CheckpointAttributionAxis == CheckpointsAxis.z) && (CheckpointAttributionDirection == CheckpointDirections.Ascending))
+        {
+            Checkpoints = FindObjectsOfType<CheckPoint>().OrderBy(o => o.transform.position.z).ToList();
+        }
+        if ((CheckpointAttributionAxis == CheckpointsAxis.z) && (CheckpointAttributionDirection == CheckpointDirections.Descending))
+{
+            Checkpoints = FindObjectsOfType<CheckPoint>().OrderByDescending(o => o.transform.position.z).ToList();
+        }
+
+        // we assign the first checkpoint 
+        CurrentCheckPoint = Checkpoints.Count > 0 ? Checkpoints[0] : null;
+    }
+
+    public void SetActiveCamera(int index)
+    {
+        vCams[index].gameObject.SetActive(true);
+    }
+}
---
+++ DysonTeleport.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Cinemachine;
+using MoreMountains.CorgiEngine;
+using MoreMountains.Tools;
+
+public class DysonTeleport : Teleporter
+{
+    public CinemachineVirtualCamera activeCam;
+    private LevelManager levelManager;
+    
+    private CinemachineVirtualCamera vcam;
+    private CinemachineVirtualCamera[] virtualCameras;  
+
+    protected override void Start()
+    {		
+        _ignoreList = new List<Transform>();
+			
+        levelManager = FindObjectOfType<LevelManager>();
+        virtualCameras = levelManager.GetComponent<DysonLevelManager>().vCams;
+
+        //virtualCameras = LevelManager.Instance.GetComponent<FollowPlayer>().vCams;
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    protected override IEnumerator TeleportEnd()
+    {
+
+
+
+        if (FadeToBlack)
+        {
+            //if (TeleportCamera)
+            //{
+            //    LevelManager.Instance.LevelCameraController.FollowsPlayer = false;
+            //}
+            MMFadeInEvent.Trigger(FadeDuration);
+            yield return new WaitForSeconds (FadeDuration);
+            //if (TeleportCamera)
+            //{
+            //    LevelManager.Instance.LevelCameraController.TeleportCameraToTarget ();
+            //    LevelManager.Instance.LevelCameraController.FollowsPlayer = true;
+					
+            //}
+            MMFadeOutEvent.Trigger(FadeDuration);
+        }
+        else
+        {
+            if (TeleportCamera)
+            {
+                LevelManager.Instance.LevelCameraController.TeleportCameraToTarget ();
+
+            }	
+        }
+
+        foreach (var vCam in virtualCameras.ToList())
+        {
+            vCam.Priority = 10;
+        }
+        activeCam.Priority = activeCam.Priority + 10;
+
+
+        //LevelManager.Instance.GetComponent<FollowPlayer>().SetActiveCamera();
+        //LevelManager.Instance.GetComponent<FollowPlayer>().SetActiveCamera();
+
+    }
+
+    //    private void OnTriggerEnter2D(Collider2D other)
+    //    {
+    //        foreach (var vCam in virtualCameras.ToList())
+    //        {
+    //            vCam.Priority = 10;
+    //        }
+    //        activeCam.Priority = 20;
+    //    }
+
+    /// <summary>
+    /// When something exits the teleporter, if it's on the ignore list, we remove it from it, so it'll be considered next time it enters.
+    /// </summary>
+    /// <param name="collider">Collider.</param>
+    protected override void OnTriggerExit2D(Collider2D collider)
+    {
+        if (_ignoreList.Contains(collider.transform))
+        {
+            _ignoreList.Remove(collider.transform);
+        }
+        base.OnTriggerExit2D(collider);
+    }
+
+}
